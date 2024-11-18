8001e281c0594132249dbd99689b4db3806a8a04
24-gautam <112795173+24-gautam@users.noreply.github.com>
2024-11-19 00:14:00 +0530

    Time: 0 ms (100%), Space: 42.3 MB (24.21%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int start = 1 , end = k , sum = 0 , n = code.length ;
+        int [] res = new int [code.length] ;
+
+        if(k == 0) return res ;
+        if(k < 0) {k = -k ; start = code.length - k ; end = code.length - 1 ;}
+
+        for(int i = start ; i <= end ; i++) sum += code[i] ;
+        for(int i = 0 ; i < code.length ; i++) {
+            res[i] = sum ; 
+            sum = sum - code[start++ % n] + code[++end % n] ;
+        } 
+        return res ;
+    }
+}
