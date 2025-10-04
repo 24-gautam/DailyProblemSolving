138de45ac2885662dafd8eba9a879048e9e8e2c5
24-gautam <112795173+24-gautam@users.noreply.github.com>
2025-10-05 00:37:52 +0530

    Time: 2381 ms (81.53%), Space: 0B (100%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,25 +1,14 @@
 /* Write your T-SQL query statement below */
 
-/* Write your T-SQL query statement below */
-
-SELECT
-DISTINCT
-    P.product_id,
-    P.product_name
-FROM
-    Product P
-INNER JOIN
-    Sales S ON P.product_id = S.product_id
-AND
-    NOT EXISTS
-        (
-            SELECT
-                1
-            FROM
-                Sales SS
-            WHERE
-                SS.product_id = S.product_id
-            AND
-               (SS.sale_date < '2019-01-01' OR SS.sale_date > '2019-03-31') 
-        )
-
+SELECT P.PRODUCT_ID, P.PRODUCT_NAME 
+FROM PRODUCT P JOIN 
+(
+    SELECT DISTINCT S1.PRODUCT_ID FROM SALES S1 
+    WHERE NOT EXISTS
+    (
+        SELECT 1 FROM SALES S2
+        WHERE (S2.SALE_DATE > '2019-03-31' OR S2.SALE_DATE < '2019-01-01') 
+        AND S1.PRODUCT_ID = S2.PRODUCT_ID
+    )
+) AS S
+ON P.PRODUCT_ID = S.PRODUCT_ID
