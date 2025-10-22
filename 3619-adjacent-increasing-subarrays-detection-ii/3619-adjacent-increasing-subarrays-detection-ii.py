a5ad3218bbd047debf4a082e917a25b8b3ac36de
24-gautam <112795173+24-gautam@users.noreply.github.com>
2025-10-22 23:07:18 +0530

    Time: 1529 ms (43.16%), Space: 45.8 MB (54.95%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.py
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.py
@@ -1,25 +1,11 @@
 class Solution:
     def maxIncreasingSubarrays(self, nums: List[int]) -> int:
-        maxIncSubArr , k = [1] * len(nums) , 1
+        prev, k, res = 0 , 1 , 0 
 
-        for i in range(len(nums) - 2, -1, -1) : 
-            if nums[i+1] > nums[i] : maxIncSubArr[i] += maxIncSubArr[i+1] 
-        
-        low , high = 0 , len(nums) // 2 
+        for i in range(1 , len(nums)) : 
+            if nums[i] > nums[i-1] : k += 1 
+            else : prev , k = k , 1 
+            
+            res = max(res , k // 2 , min(k , prev))
 
-        while low <= high : 
-            mid = low + (high - low) // 2 
-
-            if self.isValid(maxIncSubArr, mid) : 
-                k = mid
-                low = mid + 1 
-            else : 
-                high = mid - 1 
-        
-        return k
-    
-    def isValid(self, nums, k) : 
-        for i in range(len(nums) - 2 * k + 1) : 
-            if nums[i] >= k and nums[i+k] >= k : return True 
-
-        return False 
+        return res 
