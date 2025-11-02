8334dace5cfa9f8428918366b22b1e6f2dae07d7
24-gautam <112795173+24-gautam@users.noreply.github.com>
2025-11-02 21:34:57 +0530

    Time: 56 ms (66.98%), Space: 58.6 MB (41.86%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.py
@@ -5,17 +5,21 @@
 #         self.next = next
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        nums = set(nums) 
-        newHead = ListNode()
-        newHead.next = head   
-        dummy = newHead 
 
-        while dummy and dummy.next : 
-            if dummy.next.val in nums : 
-                temp = dummy
-                while temp.next and temp.next.val in nums : 
-                    temp = temp.next 
-                dummy.next = temp.next
-            dummy = dummy.next
-        
-        return newHead.next
+        nums = set(nums)
+
+        dummy = ListNode()
+        dummy.next = head
+        prev = dummy
+        curr = head
+
+        while curr:
+
+            if curr.val in nums:
+                prev.next = curr.next
+            else:
+                prev = prev.next
+
+            curr = curr.next
+
+        return dummy.next
