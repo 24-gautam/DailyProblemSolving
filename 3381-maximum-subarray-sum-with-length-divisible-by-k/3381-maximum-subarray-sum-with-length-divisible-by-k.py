4738ce26535f04ccaeaf2dd0ebd4142f0282a5a5
24-gautam <112795173+24-gautam@users.noreply.github.com>
2025-12-02 21:57:02 +0530

    Time: 375 ms (16.94%), Space: 46.1 MB (14.73%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.py
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.py
@@ -2,11 +2,12 @@ class Solution:
     def maxSubarraySum(self, nums: List[int], k: int) -> int:
         prefix_sum = [0] + list(accumulate(nums))
         ans = -math.inf
+        print(prefix_sum)
         for j in range(k):
             sum = 0
             for i in range(j, len(prefix_sum) - k, k):
                 sum = max(
-                    prefix_sum[i + k] - prefix_sum[i],
+                    prefix_sum[i + k] - prefix_sum[i], 
                     sum + prefix_sum[i + k] - prefix_sum[i]
                 )
                 ans = max(ans, sum)
